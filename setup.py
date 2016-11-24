cc1bae06c2354b2155a4bb7f2971e40f8b783dc8
Lorenz Diener <halcyon@halcy.de>
2016-11-24 20:40:23 +0100

    Preparation for pypi packaging

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(name='Mastodon.py',
+      version='1.0.0',
       description='Python wrapper for the Mastodon API',
       packages=['mastodon'],
-      install_requires=['requests'])
+      install_requires=['requests'],
+      url='https://github.com/halcy/Mastodon.py',
+      author='Lorenz Diener',
+      author_email='lorenzd+mastodonpypypi@gmail.com',
+      license='MIT',
+      keywords='mastodon api microblogging',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'Topic :: Communications',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+      ]
+)
