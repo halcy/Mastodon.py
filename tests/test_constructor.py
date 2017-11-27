e8d10795621730ffdab5be67c913d68e5d9abe07
codl <codl@codl.fr>
2017-11-27 17:47:15 +0100

    constructor tests: create credential files at runtime

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -2,10 +2,14 @@ import pytest
 from mastodon import Mastodon
 from mastodon.Mastodon import MastodonIllegalArgumentError
 
-def test_constructor_from_filenames():
+def test_constructor_from_filenames(tmpdir):
+    client = tmpdir.join('client')
+    client.write_text('foo\nbar\n', 'UTF-8')
+    access = tmpdir.join('access')
+    access.write_text('baz\n', 'UTF-8')
     api = Mastodon(
-            'tests/client.credentials',
-            access_token = 'tests/access.credentials')
+            str(client),
+            access_token = str(access))
     assert api.client_id == 'foo'
     assert api.client_secret == 'bar'
     assert api.access_token == 'baz'
