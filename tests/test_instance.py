d09d27cdd25a6277333b65953d546bd281e380da
codl <codl@codl.fr>
2017-11-27 02:37:02 +0100

    add a test for instance

---
+++ tests/test_instance.py
@@ -0,0 +1,11 @@
+from .fixtures import *
+import pytest
+
+@pytest.mark.vcr()
+def test_instance(mastodon):
+    instance = mastodon.instance()
+
+    assert isinstance(instance, dict)  # hehe, instance is instance
+
+    expected_keys = set(('description', 'email', 'title', 'uri', 'version', 'urls'))
+    assert set(instance.keys()) >= expected_keys
