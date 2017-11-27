8c9dcbe8710a425abee860affbd8951cf19609e1
codl <codl@codl.fr>
2017-11-27 20:51:12 +0100

    shouting!!!!!! ahhh python 2 is bad

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,12 +4,12 @@ from mastodon.Mastodon import MastodonIllegalArgumentError
 
 def test_constructor_from_filenames(tmpdir):
     client = tmpdir.join('client')
-    client.write_text('foo\nbar\n', 'UTF-8')
+    client.write_text(u'foo\nbar\n', 'UTF-8')
     access = tmpdir.join('access')
-    access.write_text('baz\n', 'UTF-8')
+    access.write_text(u'baz\n', 'UTF-8')
     api = Mastodon(
             str(client),
-            access_token = str(access))
+            access_token=str(access))
     assert api.client_id == 'foo'
     assert api.client_secret == 'bar'
     assert api.access_token == 'baz'
