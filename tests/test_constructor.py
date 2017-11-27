ee802d39d0873a15a26ebca9110a55bd27b696d7
codl <codl@codl.fr>
2017-11-27 15:18:18 +0100

    add tests for constructor

---
+++ tests/test_constructor.py
@@ -0,0 +1,21 @@
+import pytest
+from mastodon import Mastodon
+from mastodon.Mastodon import MastodonIllegalArgumentError
+
+def test_constructor_from_filenames():
+    api = Mastodon(
+            'client.credentials',
+            access_token = 'access.credentials')
+    assert api.client_id == 'foo'
+    assert api.client_secret == 'bar'
+    assert api.access_token == 'baz'
+
+def test_constructor_illegal_ratelimit():
+    with pytest.raises(MastodonIllegalArgumentError):
+        api = Mastodon(
+                'foo', client_secret='bar',
+                ratelimit_method='baz')
+
+def test_constructor_missing_client_secret():
+    with pytest.raises(MastodonIllegalArgumentError):
+        api = Mastodon('foo')
