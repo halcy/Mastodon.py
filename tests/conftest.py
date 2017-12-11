8a8c62a8e787a3a56cff5ebc1cd25075d4cd7712
Lorenz Diener <lorenz.diener@uni-bremen.de>
2017-12-11 14:52:03 +0100

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
-
 def _api(access_token='__MASTODON_PY_TEST_ACCESS_TOKEN'):
     import mastodon
     return mastodon.Mastodon(
             api_base_url='http://localhost:3000',
             client_id='__MASTODON_PY_TEST_CLIENT_ID',
             client_secret='__MASTODON_PY_TEST_CLIENT_SECRET',
-            access_token=access_token)
+            access_token=access_token,
+            mastodon_version="2.0.0")
 
 
 @pytest.fixture
