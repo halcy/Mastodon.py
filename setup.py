16f9ffe809d0f8c7b3bc37538852a0d11672a1c3
Lorenz Diener <lorenzd@gmail.com>
2018-06-05 23:16:26 +0200

    Prepare release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ extras = {
 }
 
 setup(name='Mastodon.py',
-      version='1.2.2',
+      version='1.3.0',
       description='Python wrapper for the Mastodon API',
       packages=['mastodon'],
       setup_requires=['pytest-runner'],
