f252957de263b772c13b1adebf8b915116ddb688
Lorenz Diener <ldiener@uni-bremen.de>
2019-05-31 14:45:05 +0200

    Hey readthedocs

--- mastodon/Mastodon.py
+++ mastodon/Mastodon.py
@@ -166,7 +166,7 @@ class Mastodon:
     }
     __VALID_SCOPES = ['read', 'write', 'follow', 'push'] + __SCOPE_SETS['read'] + __SCOPE_SETS['write']
         
-    __SUPPORTED_MASTODON_VERSION = "2.8.2"
+    __SUPPORTED_MASTODON_VERSION = "2.8.4"
     
     # Dict versions
     __DICT_VERSION_APPLICATION = "2.7.2"
