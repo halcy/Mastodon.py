5d7ca19c30dc6579bf29610fe815b822889978e6
halcy <halcy@ARARAGI-KUN>
2022-11-24 01:57:54 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ extras = {
 }
 
 setup(name='Mastodon.py',
-      version='1.6.3',
+      version='1.7.0',
       description='Python wrapper for the Mastodon API',
       packages=['mastodon'],
       install_requires=[
