67449481efb1fa6758b9eda24f78d1b2500a72dd
halcy <halcy@ARARAGI-KUN>
2024-02-11 23:11:38 +0200

    try that again

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ autodoc_member_order = 'bysource'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
