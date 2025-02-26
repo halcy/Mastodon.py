e206900c939570ba48f19abfe0dbe45480aa2103
Andrés Ignacio Torres <andresitorresm@gmail.com>
2025-02-26 11:49:52 -0800

    minor: remove 'unpacking' print statement altogether

--- mastodon/internals.py
+++ mastodon/internals.py
@@ -533,8 +533,6 @@ class Mastodon():
         # and we basically always want it
         for key in param_keys:
             if key in ['min_id', 'max_id', 'since_id']:
-                if self.debug_requests:
-                    print("Unpacking", key)
                 params[key] = self.__unpack_id(params[key], dateconv = dateconv, listify = False)
 
         return params
